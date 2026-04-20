d1ff58c1513063111e11a7fc5557b692889f125c
Alejandro Martin del Campo <alex.madelca@gmail.com>
2026-04-20 15:55:34 -0700

    Aura User Widget and Widget Controller

---
+++ Source/Aura/Private/UI/Widget/AuraUserWidget.cpp
@@ -0,0 +1,10 @@
+// Copyright Aesir
+
+
+#include "UI/Widget/AuraUserWidget.h"
+
+void UAuraUserWidget::SetWidgetController(UObject* InWidgetController)
+{
+	WidgetController = InWidgetController;
+	WidgetControllerSet();
+}
