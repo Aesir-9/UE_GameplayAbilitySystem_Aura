12400eda575a14929b9aa1a63302b08b8dc2013b
Alejandro Martin del Campo <alex.madelca@gmail.com>
2026-04-28 02:27:23 -0700

    Modifier Magnitude Calculations

---
+++ Source/Aura/Private/AbilitySystem/ModMagCalc/MMC_MaxMana.cpp
@@ -0,0 +1,39 @@
+// Copyright Aesir
+
+
+#include "AbilitySystem/ModMagCalc/MMC_MaxMana.h"
+#include "AbilitySystem/AuraAttributeSet.h"
+#include <Interaction/CombatInterface.h>
+
+UMMC_MaxMana::UMMC_MaxMana()
+{
+	//make sure we capture the attributes we want to use for calculations here
+	IntDef.AttributeToCapture = UAuraAttributeSet::GetIntelligenceAttribute();
+	IntDef.AttributeSource = EGameplayEffectAttributeCaptureSource::Target;
+	IntDef.bSnapshot = false;
+
+	RelevantAttributesToCapture.Add(IntDef);
+}
+
+float UMMC_MaxMana::CalculateBaseMagnitude_Implementation(const FGameplayEffectSpec& Spec) const
+{
+	//gather tag from source and target
+
+	const FGameplayTagContainer* SourceTags = Spec.CapturedSourceTags.GetAggregatedTags();
+	const FGameplayTagContainer* TargetTags = Spec.CapturedTargetTags.GetAggregatedTags();
+
+	FAggregatorEvaluateParameters EvaluationParameters;
+	EvaluationParameters.SourceTags = SourceTags;
+	EvaluationParameters.TargetTags = TargetTags;
+
+	float Int = 0.f;
+	GetCapturedAttributeMagnitude(IntDef, Spec, EvaluationParameters, Int);
+	// "Intelligence" will have the value of the vigor attribute on the target...
+	Int = FMath::Max<float>(Int, 0.f); // a clamp to make sure it's not neg
+
+	ICombatInterface* CombatInterface = Cast<ICombatInterface>(Spec.GetContext().GetSourceObject());
+	const int32 PlayerLevel = CombatInterface->GetPlayerLevel();
+
+	//now return the big brained calc
+	return 50.f + 2.5f * Int + 15.f * PlayerLevel;
+}
