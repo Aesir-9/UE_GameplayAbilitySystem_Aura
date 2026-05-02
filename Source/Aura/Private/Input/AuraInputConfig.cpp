c99269a09213c865917abdc712b353d7daa63887
Alejandro Martin del Campo <alex.madelca@gmail.com>
2026-05-01 21:04:49 -0700

    Input Config Data Asset

---
+++ Source/Aura/Private/Input/AuraInputConfig.cpp
@@ -0,0 +1,24 @@
+// Copyright Aesir
+
+
+#include "Input/AuraInputConfig.h"
+
+const UInputAction* UAuraInputConfig::FindAbilityInputActionForTag(const FGameplayTag& InputTag, bool bLogNotFound) const
+{
+	
+		for (const FAuraInputAction& Action : AbilityInputActions)
+		{
+			if (Action.InputTag == InputTag && Action.InputAction)
+			{
+				return Action.InputAction;
+			}
+		}
+
+		if (bLogNotFound)
+		{
+			UE_LOG(LogTemp, Error, TEXT("Can't find AbilityInputAction for InputTag [%s], on InputConfig [%s]"), *InputTag.ToString(),
+				*GetNameSafe(this));
+		}
+
+		return nullptr;
+}
