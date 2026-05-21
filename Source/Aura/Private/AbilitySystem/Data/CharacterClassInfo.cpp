a26fe4b29d9605e0b2e52eb93e2dee05a5b0ac10
Alejandro Martin del Campo <alex.madelca@gmail.com>
2026-05-20 19:59:20 -0700

    Character Class Info

---
+++ Source/Aura/Private/AbilitySystem/Data/CharacterClassInfo.cpp
@@ -0,0 +1,10 @@
+// Copyright Aesir
+
+
+#include "AbilitySystem/Data/CharacterClassInfo.h"
+
+FCharacterClassDefaultInfo UCharacterClassInfo::GetClassDefaultInfo(ECharacterClass CharacterClass)
+{
+
+    return CharacterClassInformation.FindChecked(CharacterClass);
+}
