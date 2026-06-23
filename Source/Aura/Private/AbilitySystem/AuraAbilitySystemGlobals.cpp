cd783d8c35b9f956a14180bb73f5f462eb51f895
Alejandro Martin del Campo <alex.madelca@gmail.com>
2026-06-22 19:58:13 -0700

    Aura Ability System Globals

---
+++ Source/Aura/Private/AbilitySystem/AuraAbilitySystemGlobals.cpp
@@ -0,0 +1,11 @@
+// Copyright Aesir
+
+
+#include "AbilitySystem/AuraAbilitySystemGlobals.h"
+
+#include "AuraAbilityTypes.h"
+
+FGameplayEffectContext* UAuraAbilitySystemGlobals::AllocGameplayEffectContext() const
+{
+	return new FAuraGameplayEffectContext();
+}
