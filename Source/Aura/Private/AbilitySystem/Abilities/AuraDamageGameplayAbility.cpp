52ad5cd0d8883b4ebb06ffb2597b783a6f6e9984
Alejandro Martin del Campo <alex.madelca@gmail.com>
2026-07-17 19:37:25 -0700

    Causing Melee Damage

--- Source/Aura/Private/AbilitySystem/Abilities/AuraDamageGameplayAbility.cpp
+++ Source/Aura/Private/AbilitySystem/Abilities/AuraDamageGameplayAbility.cpp
@@ -2,4 +2,19 @@
 
 
 #include "AbilitySystem/Abilities/AuraDamageGameplayAbility.h"
+#include "AbilitySystemBlueprintLibrary.h"
+#include "AbilitySystemComponent.h"
 
+void UAuraDamageGameplayAbility::CauseDamage(AActor* TargetActor)
+{
+	FGameplayEffectSpecHandle DamageSpecHandle = MakeOutgoingGameplayEffectSpec(DamageEffectClass, 1.f);
+	for (TTuple<FGameplayTag, FScalableFloat> Pair : DamageTypes)
+	{
+		float ScaledDamage = Pair.Value.GetValueAtLevel(GetAbilityLevel());
+		UAbilitySystemBlueprintLibrary::AssignTagSetByCallerMagnitude(DamageSpecHandle, Pair.Key, ScaledDamage);
+	}
+
+	//rememeber to dereference "Data" which is the gameplay effect spec and is a pointer
+	GetAbilitySystemComponentFromActorInfo()->ApplyGameplayEffectSpecToTarget(*DamageSpecHandle.Data.Get(), UAbilitySystemBlueprintLibrary::GetAbilitySystemComponent(TargetActor));
+
+}
